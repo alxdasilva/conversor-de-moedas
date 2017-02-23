4985b45972d125eb0793a3cf4329105849d79a63
alxdasilva <alx.silva@live.com>
2017-02-23 14:31:53 -0300

    change to self = this

--- conversor-de-moeda/app.js
+++ conversor-de-moeda/app.js
@@ -1,30 +1,30 @@
 angular.module("converter", [])
-	.controller("ConverterCtrl", ["$http", "$scope", function ($http, $scope) {
-		$scope.selected = "EUR";
+	.controller("ConverterCtrl", ["$http", function ($http) {
+        var self = this;
 		var baseUrl = "http://api.fixer.io/latest";
 		//para pegar a lista de moedas a base é EUR (preguiça de criar a lista)
 		// to get list of all coin EUR is the base (is here because I dont wanna create a full list with all names)
 		$http.get(baseUrl).then(function (response2) {
-			$scope.currency = response2.data;
+			self.currency = response2.data;
 		}, function (err) {
 			console.log(err);
 		});
 
 		//Para selecionar a base da moeda e fazer o calculo da conversão 
 		//To select coin base and calculate the exchange
-		$scope.result = function () {
-			var changeBaseUrl = "http://api.fixer.io/latest?base=" + $scope.selectedCurrency;
+		self.result = function () {
+			var changeBaseUrl = "http://api.fixer.io/latest?base=" + self.selectedCurrency;
 			$http.get(changeBaseUrl).then(function (response) {
 				console.log(response.data);
-				$scope.currency2 = response.data.rates[$scope.selectedCurrency2] * $scope.number;
+				self.currency2 = response.data.rates[self.selectedCurrency2] * self.number;
 			}, function (err) {
 				console.log(err);
 			});
 		};
 
-		$scope.flipModel = function () {
-			var model1 = $scope.selectedCurrency;
-			$scope.selectedCurrency = $scope.selectedCurrency2;
-			$scope.selectedCurrency2 = model1;
+		self.flipModel = function () {
+			var model1 = self.selectedCurrency;
+			self.selectedCurrency = self.selectedCurrency2;
+			self.selectedCurrency2 = model1;
 		};
 	}]);
